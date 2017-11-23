c38c8c89179735df74b4bc9e4717c6d85efaae60
HaiwangYu <yuhw.pku@gmail.com>
2017-11-23 00:52:30 -0500

    init

---
+++ art-seaquest/DataProducts/classes.h
@@ -0,0 +1,9 @@
+//
+// Build a dictionary.
+//
+
+#include "art-seaquest/DataProducts/EventSummary.h"
+
+#include "art/Persistency/Common/Wrapper.h"
+
+template class art::Wrapper<tex::EventSummary>;
