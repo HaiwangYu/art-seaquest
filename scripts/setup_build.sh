1bfe08669b1a0d09a3233ae299e28b16b7f207e2
HaiwangYu <yuhw.pku@gmail.com>
2017-11-25 18:09:44 -0500

    only setup_dev

--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-source /grid/fermiapp/products/uboone/setup_uboone.sh
-
-export ART_SEAQUEST_QUAL=e9:nu:s30 # workbook 0.91 
 source /e906/app/users/yuhw/art-seaquest/art-seaquest/ups/setup_for_development -p $ART_SEAQUEST_QUAL
