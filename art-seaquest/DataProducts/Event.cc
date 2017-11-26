886432f742b1571a4ae622097b28bface3e64858
HaiwangYu <yuhw.pku@gmail.com>
2017-11-26 14:18:48 -0500

    add full info to Event

--- art-seaquest/DataProducts/Event.cc
+++ art-seaquest/DataProducts/Event.cc
@@ -10,17 +10,37 @@
 namespace seaquest {
 
 Event::Event():
-  _event_id(std::numeric_limits<int>::max()) {
-}
+		_run_id(std::numeric_limits<int>::max()),
+		_spill_id(std::numeric_limits<int>::max()),
+		_event_id(std::numeric_limits<int>::max()),
+		_coda_event_id(std::numeric_limits<int>::max()),
+		_trigger(0),
+		_data_quality(std::numeric_limits<int>::max()),
+		_vme_time(std::numeric_limits<int>::max())
+		{
+			for(int i=0;i<5;++i) _raw_matrix[i] = 0;
+			for(int i=0;i<5;++i) _after_inh_matrix[i] = 0;
+		}
 
 #ifndef __GCCXML__
 
 std::ostream& operator<<(std::ostream& ost,
                          const seaquest::Event& event){
-  ost << " ( Event Summary: "
-      << " EventID: " << event.get_event_id()
-			<< " )";
-
+  ost << "---seaquest::Event:--------------------------" << std::endl;
+  ost
+  << "runID: " << event.get_run_id()
+  << "spillID: " << event.get_spill_id()
+  << "eventID: " << event.get_event_id()
+  << "codaEventID: " << event.get_coda_event_id()
+  << std::endl;
+  for(int i = seaquest::Event::NIM1; i<=seaquest::Event::NIM5; ++i) {
+	  ost <<"NIM"<<i<<": " << event.get_trigger(static_cast<seaquest::Event::TriggerMask>(i)) << "; ";
+  }
+  ost << std::endl;
+  for(int i = seaquest::Event::MATRIX1; i<=seaquest::Event::MATRIX5; ++i){
+	  ost <<"MATRIX"<<i<<": " << event.get_trigger(static_cast<seaquest::Event::TriggerMask>(i)) << "; ";
+  }
+  ost << std::endl;
   return ost;
 }
 }
--- art-seaquest/DataProducts/Event.h
+++ art-seaquest/DataProducts/Event.h
@@ -6,6 +6,8 @@
 //
 
 #include <iosfwd>
+#include <iostream>
+#include <limits>
 
 namespace seaquest {
 
@@ -13,20 +15,87 @@ namespace seaquest {
 
   public:
 
+  	enum TriggerMask {
+  		NIM1 = 0,
+  		NIM2 = 1,
+  		NIM3 = 2,
+  		NIM4 = 3,
+  		NIM5 = 4,
+  		MATRIX1 = 5,
+  		MATRIX2 = 6,
+  		MATRIX3 = 7,
+  		MATRIX4 = 8,
+  		MATRIX5 = 9
+  	};
+
     Event();
 
 #ifndef __GCCXML__
 #endif  // __GCCXML__
+
+
+  	void identify(std::ostream& os = std::cout) const;
+
+  	int get_run_id() const {return _run_id;}
+  	void set_run_id(const int a) {_run_id = a;}
+
+  	int get_spill_id() const {return _spill_id;}
+  	void set_spill_id(const int a) {_spill_id = a;}
+
   	int get_event_id() const {return _event_id;}
   	void set_event_id(const int a) {_event_id = a;}
 
+  	int get_coda_event_id() const {return _coda_event_id;}
+  	void set_coda_event_id(const int a) {_coda_event_id = a;}
+
+
+  	int get_data_quality() const {return _data_quality;}
+  	void set_data_quality(const int a) {_data_quality = a;}
+
+  	int get_vme_time() const {return _vme_time;}
+  	void set_vme_time(const int a) {_vme_time = a;}
+
+  	bool get_trigger(const seaquest::Event::TriggerMask i) const {return (_trigger&(1<<i)) > 0 ;}
+  	void set_trigger(const seaquest::Event::TriggerMask i, const bool a) {a ? (_trigger |= (1<<i)) : (_trigger &= ~(1<<i)) ;}
+
+  	int get_raw_matrix(const unsigned short i) const {
+  		if(i<5) return _raw_matrix[i];
+  		return std::numeric_limits<int>::max();
+  	}
+  	void set_raw_matrix(const unsigned short i, const bool a) {
+  		if(i<5) _raw_matrix[i] = a;
+  		else std::cout<<"SQEvent_v1::set_raw_matrix: i>=5";
+  	}
+
+  	int get_after_inh_matrix(const unsigned short i) const {
+  		if(i<5) return _after_inh_matrix[i];
+  		return std::numeric_limits<int>::max();
+  	}
+  	void set_after_inh_matrix(const unsigned short i, const bool a) {
+  		if(i<5) _after_inh_matrix[i] = a;
+  		else std::cout<<"SQEvent_v1::set_after_inh_matrix: i>=5";
+  	}
+
+
   private:
-    int _event_id;
+  	int _run_id;
+  	int _spill_id;
+  	int _event_id;
+  	int _coda_event_id;
+
+  	unsigned short _trigger; //< NIM[1-5], MATRIX[1-5]
+
+  	int _raw_matrix[5];
+
+  	int _after_inh_matrix[5];
+
+  	int _data_quality;
+
+  	int _vme_time;
   };
 
 #ifndef __GCCXML__
-  std::ostream& operator<<(std::ostream& ost,
-                           const seaquest::Event& event );
+  std::ostream& operator<<(std::ostream& ost, const seaquest::Event& event );
 #endif  // __GCCXML__
 
 }
